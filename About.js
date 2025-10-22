6b4fa08f3e5f40f76a8e0529cbb34f5244d3086b
Deetcitha sree <dheepapu@gmail.com>
2025-10-22 12:34:02 +0530

    filemanager src/
    Add About component with welcome message and description

---
+++ About.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+function About() {
+  return (
+    <div className="welcome-container">
+      <h1>About</h1>
+      <p>
+        This app allows users to log in, upload files, and manage uploads easily
+        with a simple and user-friendly interface.
+      </p>
+    </div>
+  );
+}
+
+export default About;
