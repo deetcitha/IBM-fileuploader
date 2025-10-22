2b024c8e89386fc3e2c9c42590645ccd9ce22a31
Deetcitha sree <dheepapu@gmail.com>
2025-10-22 12:44:11 +0530

    Add files via upload

---
+++ welcome.js
@@ -0,0 +1,11 @@
+import React,{component} from 'react';
+class Welcome extends component {
+  render() {
+    return (
+      <div>
+        <h2>Hello from javascript component</h2>
+        <p> This is a simple example.</p>
+      </div>
+    );
+  }
+}
